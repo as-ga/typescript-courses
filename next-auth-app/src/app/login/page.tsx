332b1d1610283e67b1dd4e44957a105af6bc3aed
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-03-30 21:15:44 +0000

    Add login page component

---
+++ next-auth-app/src/app/login/page.tsx
@@ -0,0 +1,69 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+export default function LoginPage() {
+  const router = useRouter();
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [loding, setLoading] = useState(false);
+
+  const onLogin = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("api/users/login", user);
+      console.log("Login success", response.data);
+      toast.success("Login success");
+      router.push("/profile");
+    } catch (error: any) {
+      console.log(error);
+      toast.error("Login failed", error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>{loding ? "Processing..." : "Login"}</h1>
+
+      <label htmlFor="email">email</label>
+      <input
+        id="email"
+        type="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="email"
+      />
+      <label htmlFor="password">password</label>
+      <input
+        id="password"
+        type="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="password"
+      />
+      <button
+        onClick={onLogin}
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      >
+        {buttonDisabled ? "Fill all fields" : "Login"}
+      </button>
+      <Link href="/signup"> Visit Signup Page </Link>
+    </div>
+  );
+}

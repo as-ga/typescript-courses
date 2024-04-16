b24d6b52319f6e6adec086935a637cfb6606662e
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-04-16 07:40:27 +0000

    Add AuthProvider component for authentication context

--- mystery-message/src/app/layout.tsx
+++ mystery-message/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import AuthProvider from "@/context/AuthProvider";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <AuthProvider>
+        <body className={inter.className}>{children}</body>
+      </AuthProvider>
     </html>
   );
 }

1956ca9cc4f57945bc8a20f6db3bf1cc930bbaa9
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-04-16 17:29:46 +0000

    Add route for checking username uniqueness

---
+++ mystery-message/src/app/api/check-username-unique/route.ts
@@ -0,0 +1,55 @@
+import dbConnect from "@/lib/dbConnect";
+import { UserModel } from "@/model/user.model";
+import { z } from "zod";
+import { usernameValidation } from "@/schemas/signUpSchema";
+
+const UsernameQuerySchema = z.object({
+  username: usernameValidation,
+});
+
+export async function GET(request: Request) {
+  await dbConnect();
+
+  try {
+    const { searchParams } = new URL(request.url);
+    const queryParam = { username: searchParams.get("username") };
+
+    //  Validate USERNAME with zod
+    const result = UsernameQuerySchema.safeParse(queryParam);
+    if (!result.success) {
+      const usernameErrors = result.error.format().username?._errors || [];
+      return Response.json(
+        {
+          success: false,
+          message:
+            usernameErrors?.length > 0
+              ? usernameErrors.join(", ")
+              : "Invalid query parameters",
+        },
+        { status: 400 }
+      );
+    }
+
+    const { username } = result.data;
+
+    const existingVerifiedUser = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
+
+    if (existingVerifiedUser) {
+      return Response.json(
+        { success: false, message: "Username already exists" },
+        { status: 409 }
+      );
+    }
+
+    return Response.json({ success: true, message: "Username is unique" });
+  } catch (error) {
+    console.error("Error checking username unique: ", error);
+    return Response.json(
+      { success: false, message: "Error checking username unique" },
+      { status: 500 }
+    );
+  }
+}

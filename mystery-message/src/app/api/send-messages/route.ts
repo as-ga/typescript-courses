7c38656f31a73236637ddeb7235207298cef0e5c
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-04-18 19:05:33 +0000

    Add API route for sending messages

---
+++ mystery-message/src/app/api/send-messages/route.ts
@@ -0,0 +1,52 @@
+import dbConnect from "@/lib/dbConnect";
+import { UserModel } from "@/model/user.model";
+import { Message } from "@/model/user.model";
+
+export async function POST(request: Request) {
+  await dbConnect();
+
+  const { username, content } = await request.json();
+
+  try {
+    const user = await UserModel.findOne({ username });
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        { status: 401 }
+      );
+    }
+
+    // is user accepting messages?
+    if (!user.isAcceptingMessage) {
+      return Response.json(
+        {
+          success: false,
+          message: "User is not accepting messages",
+        },
+        { status: 403 }
+      );
+    }
+    const newMessage = { content, createdAt: new Date() };
+    user.messages.push(newMessage as Message);
+    await user.save();
+    return Response.json(
+      {
+        success: true,
+        message: "Message sent successfully",
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log("failed to send message", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Failed to send message",
+      },
+      { status: 500 }
+    );
+  }
+}

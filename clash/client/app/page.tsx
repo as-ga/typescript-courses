9c30acd3348f5e2762dce3e0f2b72a2903c936b1
mr.gaurav9554 <mr.gaurav9554@gmail.com>
2024-08-29 14:15:41 +0000

    clash client side setup

---
+++ clash/client/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>This is Home Page</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+}

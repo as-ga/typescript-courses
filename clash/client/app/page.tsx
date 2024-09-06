aa3671c3fdd4550d226acdc30d257f29a7db2641
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-09-06 16:34:00 +0000

    feat: send mail

--- clash/client/app/page.tsx
+++ clash/client/app/page.tsx
@@ -1,8 +1,10 @@
+import HeroSection from "@/components/base/HeroSection";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
+      <HeroSection />
       <h1>This is Home Page</h1>
       <Button>Click Me</Button>
     </div>
--- clash/server/index.ts
+++ clash/server/index.ts
@@ -1,11 +1,28 @@
 import express, { Application, Request, Response } from "express";
 import "dotenv/config";
+import path from "path";
+import { fileURLToPath } from "url";
+import { sendEmail } from "./config/mail.js";
+import ejs from "ejs";
 
 const app: Application = express();
 const port: number = Number(process.env.PORT || 5000);
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-app.get("/", (req: Request, res: Response) => {
-  return res.send("Hello It's Working");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// set view engine
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./"));
+
+app.get("/", async (req: Request, res: Response) => {
+  const html = await ejs.renderFile(__dirname + `/emails/welcome.ejs`, {
+    name: "Ashutosh Gaurav",
+  });
+  await sendEmail("geyeyev843@obisims.com", "Testing SMTP", html);
+  // return res.render("emails/welcome", { name: "Ashutosh Gaurav" });
+  return res.json({ message: "Email sent " });
 });
 
 app.listen(port, () => {
